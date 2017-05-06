52564c351b4bdec87f815510ed406526717301b0
Vaclav Klecanda <vencax77@gmail.com>
2017-05-06 23:49:33 +0200

    silence!

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require('dotenv').config({silent: true})
 const express = require('express')
 const simpleOauthModule = require('simple-oauth2')
 const randomstring = require('randomstring')
